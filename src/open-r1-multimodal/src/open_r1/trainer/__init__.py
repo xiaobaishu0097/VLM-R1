efd440341422f7a5a148065e036cac295c9c8e3f
Ruochen Xu <xu_ruochen@hzlh.com>
2025-02-18 15:52:05 +0800

    add modified vllm trainer from FUJIsyu0515

--- src/open-r1-multimodal/src/open_r1/trainer/__init__.py
+++ src/open-r1-multimodal/src/open_r1/trainer/__init__.py
@@ -1,4 +1,5 @@
 from .grpo_trainer import Qwen2VLGRPOTrainer
-from .vllm_grpo_trainer import Qwen2VLGRPOVLLMTrainer 
+from .vllm_grpo_trainer import Qwen2VLGRPOVLLMTrainer
+from .vllm_grpo_trainer_modified import Qwen2VLGRPOVLLMTrainerModified
 
-__all__ = ["Qwen2VLGRPOTrainer", "Qwen2VLGRPOVLLMTrainer"]
+__all__ = ["Qwen2VLGRPOTrainer", "Qwen2VLGRPOVLLMTrainer", "Qwen2VLGRPOVLLMTrainerModified"]
